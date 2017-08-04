fd1bb6d124857598ef7a703ae8142f7b3c5e182a
Daniel James Tarplett <danieljamestarplett@hotmail.com>
2017-08-04 19:47:41 +0200

    Added main program and program for handling graphics

---
+++ HealthCareDatabase/src/GraphicsMenu.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.*;
+
+/**
+ * The class defines the graphical user interface for the
+ * healthcare database program.
+ *  
+ * @author Daniel James Tarplett
+ *
+ */
+public class GraphicsMenu extends Application{
+	
+	public void start(Stage window) throws Exception{
+	}
+	
+}
---
+++ HealthCareDatabase/src/HealthCareDatabase.java
@@ -0,0 +1,16 @@
+
+/**
+ * The HealthCareDatabase program runs the whole system of classes
+ * in a fully working database program.
+ * 
+ * @author Daniel James Tarplett
+ *
+ */
+public class HealthCareDatabase {
+	public static void main(String[] args){
+		// First, try to run the graphics Menu, if not,
+		// run terminal based menu
+		
+		// try to read the file from 
+	}
+}
